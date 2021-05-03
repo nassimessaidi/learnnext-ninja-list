74b9652d2aebe45d4a3671f81afc16a447cd35e9
Nassim Essaidi <contact@essaidi.me>
2021-05-03 16:39:16 +0000

    fetching data (getStaticProps)

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,13 +1,31 @@
 import Head from "next/head";
+import styles from "../../styles/ninjas.module.css";
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Home = ({ ninjas }) => {
   return (
     <>
       <Head>
         <title>Ninja List | Listing</title>
         <meta name="keywords" content="ninjas" />
       </Head>
-      <h1>Ninjas</h1>
+      <h1 className={styles.title}>All Ninjas</h1>
+
+      {ninjas.map((ninja) => (
+        <div key={ninja.id} className="">
+          <a className={styles.single}>
+            <h4>{ninja.name}</h4>
+          </a>
+        </div>
+      ))}
     </>
   );
 };
