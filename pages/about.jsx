ce02ce02c10fd3d22e716db408460c39aafeaf46
Nassim Essaidi <contact@essaidi.me>
2021-05-03 15:59:54 +0000

    styles, components, routes, custom 404 page

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis veniam
+        iure perspiciatis eveniet repellendus eum delectus quasi laboriosam, ea,
+        ex quo error a ratione cumque consequuntur adipisci molestiae odio
+        repudiandae?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Ninjas</h1>;
+};
+
+export default Home;
